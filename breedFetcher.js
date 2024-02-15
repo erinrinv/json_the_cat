b01b0742622f2271f5f7cdd295f820d3f8551d00
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-15 13:01:08 -0700

    testing cat as a service work. Created index.js, as well as test folder and updated package-json, moved main logic from breedfetcher into index.js for users to use

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,30 +2,38 @@
 const request = require("request");
 
 const fs = require("fs");
-// gets breed based on command line argument when running
-const host = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`;
+
 
 //reads the body of the response asynchronously
-const breedFetcher = function (callback) {
+// Take name of the cat and then once its found bring description back
+const fetchBreedDescription = function(breedName, callback) {
+  // Checks API
+  const host = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  // Aysnc function checks JSON file based on pull from API
+  // If not found will return message
+  // otherwise returns first matching of breed of cat
+  const async = function(body) {
+    const content = JSON.parse(body);
+    if (content.length === 0) {
+      return 'cat not found!!';
+    } else {
+      return content[0]['description'];
+    }
+  };
+  // Checks for errors
+  // If there are errors calls with error
+  //fetch the description of a given cat breed from the Cat API and invoke a callback function with the result.
   request(host, (error, response, body) => {
-    if (error) console.log(error);
-    fs.readFile(body, "utf8", (error, data) => {
-      callback(body);
+    if (error) callback(error, null);
+    fs.readFile(body, 'utf8', () => {
+      const desc = async(body);
+      callback(null, desc);
     });
   });
 };
 
-// reads the JSON data and checks if the response contains any data
-// if no data is found then says "cat can not be found"
-// if found then logs the description of the cat
-const apiCallback = function (body) {
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("cat not found!!");
-  } else {
-    console.log(data[0]["description"]);
-  }
-};
 
-// calls the function and begins the process
-breedFetcher(apiCallback);
+
+// export
+module.exports = { fetchBreedDescription };
